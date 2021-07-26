f3d4906a2bf983bb939719a6c7e66e9393a4f6b3
周美磊 <zhoumeilei@sfmail.sf-express.com>
2021-07-26 20:40:16 +0800

    增加element-plus,完善类型支持

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
-import { createApp } from 'vue'
-import App from './App'
+import { createApp } from 'vue';
+import App from './App';
 import router from '@/router'
-import store from '@/store'
-import globalData from '@/globalData'
+import store from '@/store';
+import globalData from '@/globalData';
+import ElementPlus from 'element-plus';
+import 'element-plus/lib/theme-chalk/index.css';
 
-createApp(App).use(router).use(store).use(globalData).mount('#app')
+createApp(App).use(ElementPlus).use(router).use(store).use(globalData).mount('#app')
--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -2,6 +2,7 @@ import { computed, ref, watchEffect, defineComponent } from 'vue'
 // import store from '@/store'
 import router from '@/router'
 import globalData from '@/globalData'
+import { ElOption, ElSelect } from 'element-plus'
 
 export default defineComponent({
   name: 'Index',
@@ -13,17 +14,26 @@ export default defineComponent({
 
     let greet = ref(1)
     let aa = ref(1)
-    setInterval(() => {
-      greet.value = greet.value + 1
-    }, 1000)
+    // setInterval(() => {
+    //   greet.value = greet.value + 1
+    // }, 1000)
     let fun = () => {
       console.log(aa, 'from home component')
     }
     globalData.pageFunction.index = fun
     // provide('fun', fun)
-
-    
+    const selectVal = ref<string>("1")
+    const inputVal = ref<string>("")
+    watchEffect(() => {
+      console.log(selectVal.value);
+    })
     return () => (<>
+      <ElSelect defaultFirstOption v-model={selectVal.value}>
+        <ElOption value="1" label="aaaa"></ElOption>
+        <ElOption value="2" label="abbba"></ElOption>
+        <ElOption value="3" label="aarfrdfcr"></ElOption>
+      </ElSelect>
+      {/* <ElInput change={(val: any) => console.log(val)} v-model={inputVal} /> */}
       greet1: {greet.value}
       <div onClick={() => go('/home')}>here is index page, click to home</div>
       aa: {aa.value}
